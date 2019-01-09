8976ee5df75ffd37f115a1bc016b3e153db9eb6b
Thimal Wickremage <thimalmadushanka@gmail.com>
2019-01-09 18:33:12 +0530

    Code formatting

--- api/db.js
+++ api/db.js
@@ -1,18 +1,18 @@
 const Sequelize = require('sequelize');
 
 const db = new Sequelize('invoicealot', 'root', 'ilash', { // TODO: get details from env/conf
-    host: '127.0.0.1',
-    dialect: 'mysql'
+  host: '127.0.0.1',
+  dialect: 'mysql'
 });
 
 // test db connection
 db
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+  .authenticate()
+  .then(() => {
+      console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+      console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = db;
